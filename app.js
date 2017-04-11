bcda9c30acc0b2fd4590126a863c9fe5dbf12664
liming <360622464@qq.com>
2017-04-12 03:23:28 +0800

    增加common.css   可以对header footer 直接引用

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var detail = require('./routes/detail');
 var rooms = require('./routes/rooms');
+var intro = require('./routes/intro');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/detail', detail);
 app.use('/rooms', rooms);
+app.use('/intro', intro);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
