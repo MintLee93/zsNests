5447f588a55178fdf69b20a9d1632a351b5fbe93
liming <360622464@qq.com>
2017-04-12 03:36:31 +0800

    增加五个页面的路由  以及html 基础部分

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 var detail = require('./routes/detail');
 var rooms = require('./routes/rooms');
 var intro = require('./routes/intro');
+var service = require('./routes/service');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/users', users);
 app.use('/detail', detail);
 app.use('/rooms', rooms);
 app.use('/intro', intro);
+app.use('/service', service);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/service.js
@@ -0,0 +1,14 @@
+/**
+ * Created by RM on 2017/4/12.
+ */
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('service');
+});
+
+module.exports = router;
